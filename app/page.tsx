2a8867b315e70849dcff078806a57ccb01a11d10
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2023-12-16 15:02:21 +0100

    feat(construction): add footer

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import LinkedIn from "@/public/icons/linkedin.svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <>
       <header className="flex flex-row justify-between pt-10 px-32 items-center w-full">
         <div>
           <Image src={Logo} height={100} width={100} alt="Logo" />
@@ -21,41 +21,57 @@ export default function Home() {
           </Link>
         </Button>
       </header>
-      <Image
-        className="mt-14"
-        src={Construction}
-        height={250}
-        alt="Under Construction"
-      />
-      <h1 className="text-6xl font-bold mt-8">
-        Error 503: <span className="text-accent">Under Construction</span>
-      </h1>
-      <p className="max-w-3xl text-center text-xl mt-6">
-        Bald gibt es hier mehr zu sehen als nur virtuellen Baustaub. Ich arbeite
-        gerade daran meine Website auf den neuesten Stand zu bringen.
-      </p>
+      <main className="flex flex-col items-center">
+        <Image
+          className="mt-14"
+          src={Construction}
+          height={250}
+          alt="Under Construction"
+        />
+        <h1 className="text-6xl font-bold mt-8">
+          Error 503: <span className="text-accent">Under Construction</span>
+        </h1>
+        <p className="max-w-3xl text-center text-xl mt-6">
+          Bald gibt es hier mehr zu sehen als nur virtuellen Baustaub. Ich
+          arbeite gerade daran meine Website auf den neuesten Stand zu bringen.
+        </p>
 
-      <Button asChild className="rounded-3xl px-8 py-4 mt-12">
+        <Button asChild className="rounded-3xl px-8 py-4 mt-12">
+          <Link
+            href="https://cal.com/msplitt/prototype-audit"
+            className="font-semibold"
+          >
+            Get a Prototype Audit
+          </Link>
+        </Button>
+        <p className="text-gray-700 text-sm mt-2">20min MVP Feedback</p>
+        <div className="bg-black h-[146px] w-full mt-24"></div>
+        <div className="flex flex-row justify-center gap-6 mt-8 items-center">
+          <Link href="https://www.linkedin.com/in/marvin-splitt/">
+            <Image src={LinkedIn} height={24} width={24} alt="LinkedIn" />
+          </Link>
+          <Link href="https://twitter.com/marvinsplitt">
+            <Image src={Twitter} height={24} width={24} alt="Twitter" />
+          </Link>
+          <Link href="https://github.com/marvin-splitt">
+            <Image src={GitHub} height={24} width={24} alt="GitHub" />
+          </Link>
+        </div>
+      </main>
+      <footer className="flex flex-row justify-center items-center gap-4 mt-8">
         <Link
-          href="https://cal.com/msplitt/prototype-audit"
-          className="font-semibold"
+          href="https://msdev.digital/imprint"
+          className="text-gray-700 text-sm"
         >
-          Get a Prototype Audit
+          Imprint
         </Link>
-      </Button>
-      <p className="text-gray-700 text-sm mt-2">20 min MVP Feedback</p>
-      <div className="bg-black h-[146px] w-full mt-24"></div>
-      <div className="flex flex-row justify-center gap-6 mt-8">
-        <Link href="https://www.linkedin.com/in/marvin-splitt/">
-          <Image src={LinkedIn} height={24} width={24} alt="LinkedIn" />
+        <Link
+          href="https://msdev.digital/privacy"
+          className="text-gray-700 text-sm"
+        >
+          Privacy
         </Link>
-        <Link href="https://twitter.com/marvinsplitt">
-          <Image src={Twitter} height={24} width={24} alt="Twitter" />
-        </Link>
-        <Link href="https://github.com/marvin-splitt">
-          <Image src={GitHub} height={24} width={24} alt="GitHub" />
-        </Link>
-      </div>
-    </main>
+      </footer>
+    </>
   );
 }
