997dbc83beae38821c3a7835e9ba9f62e3cef307
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2023-12-28 10:37:47 +0100

    feat(hero): fix responsiveness

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -7,10 +7,12 @@ export const HeroSection: React.FC = () => {
   return (
     <div className="container flex flex-col items-center">
       <div className="relative">
-        <h1 className="text-4xl lg:text-6xl font-bold text-center mt-20">
-          Realize your product idea
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center mt-20">
+          Realize your <br className="block xs:hidden" />
+          product idea
           <br />
-          through <span className="text-accent">user-centric</span>
+          through{" "}
+          <span className="text-accent whitespace-nowrap">user-centric</span>
           <br /> prototypes
         </h1>
         <p className="max-w-3xl text-center lg:text-xl mt-6">
@@ -19,7 +21,7 @@ export const HeroSection: React.FC = () => {
           Fast, efficient and without unnecessary costs.
         </p>
         <Image
-          className="absolute h-9 w-9 top-16 -right-14"
+          className="absolute h-6 w-6 md:h-9 md:w-9 top-20 xs:-right-10 md:top-16 -right-4 md:-right-14"
           src={Arrow}
           alt="Arrow Icon"
         />
