e2163a08b40aa14e0070a9e1438c53cf42bf44c8
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2023-12-30 13:48:54 +0100

    feat: remove social link section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import Image from "next/image";
 import { LogoSection } from "@/components/logo-section";
 import { HeroSection } from "@/components/hero-section";
-
-// Icons
-import Link from "next/link";
-import GitHub from "@/public/icons/github.svg";
-import Twitter from "@/public/icons/twitter.svg";
-import LinkedIn from "@/public/icons/linkedin.svg";
 import { BenefitsSection } from "@/components/benefits-section";
 import { AboutSection } from "@/components/about-section";
 
@@ -17,7 +10,7 @@ export default function Home() {
       <LogoSection />
       <BenefitsSection />
       <AboutSection />
-      <div className="flex flex-row justify-center gap-6 mt-8 items-center">
+      {/* <div className="flex flex-row justify-center gap-6 mt-8 items-center">
         <Link href="https://www.linkedin.com/in/marvin-splitt/">
           <Image src={LinkedIn} height={24} width={24} alt="LinkedIn" />
         </Link>
@@ -27,7 +20,7 @@ export default function Home() {
         <Link href="https://github.com/marvin-splitt">
           <Image src={GitHub} height={24} width={24} alt="GitHub" />
         </Link>
-      </div>
+      </div> */}
     </>
   );
 }
