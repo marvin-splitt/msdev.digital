60c42eef3bd5388e67e6d7b72030bd7b6a87953c
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2024-01-02 15:31:03 +0100

    feat(workflow-section): add workflow section

---
+++ components/workflow-section.tsx
@@ -0,0 +1,77 @@
+import Image, { StaticImageData } from "next/image";
+import AnalysisIcon from "@/public/workflow/analysis.svg";
+import TargetGroupIcon from "@/public/workflow/target-group.svg";
+import RoadmapIcon from "@/public/workflow/roadmap.svg";
+import MvpIcon from "@/public/workflow/mvp.svg";
+import ImplementationIcon from "@/public/workflow/implementation.svg";
+
+type WorkflowStep = {
+  title: string;
+  description: string;
+  image: StaticImageData;
+};
+
+const workflowSteps: WorkflowStep[] = [
+  {
+    title: "Analysis",
+    description:
+      "Together we identify opportunities and pain points for your product in line with your vision and the market.",
+    image: AnalysisIcon,
+  },
+  {
+    title: "Validate target group",
+    description:
+      "User feedback is integrated into the product strategy to ensure that the MVP is perfectly tailored to the needs of your target group.",
+    image: TargetGroupIcon,
+  },
+  {
+    title: "Build roadmap",
+    description:
+      "We create a clear roadmap for your project, which serves as a transparent guide for structured implementation.",
+    image: RoadmapIcon,
+  },
+  {
+    title: "Define MVP",
+    description:
+      "Clearly defined requirements for your MVP enable a streamlined development process for a fast market launch.",
+    image: MvpIcon,
+  },
+  {
+    title: "Implementation",
+    description:
+      "Through structured and iterative development cycles, we build the features that are most important to your users and thus create a product that they really love.",
+    image: ImplementationIcon,
+  },
+];
+
+export const WorkflowSection: React.FC = () => {
+  return (
+    <div className="container flex flex-col items-center">
+      <h2 className="font-semibold text-2xl md:text-4xl text-center max-w-3xl">
+        What a collaboration can look like
+      </h2>
+      <p className="max-w-3xl text-center text-lg md:text-xl mt-5">
+        Each project is unique, but a typical workflow could look like this...
+      </p>
+      <div className="flex flex-col md:flex-row gap-5 mt-10">
+        {workflowSteps.map((step) => (
+          <div key={step.title} className="flex flex-col">
+            <Image
+              src={step.image}
+              alt={step.title}
+              className="h-16 w-fit object-contain"
+            />
+
+            <p className="font-semibold text-lg md:text-xl mt-3">
+              {step.title}
+            </p>
+
+            <p className="max-w-xs md:max-w-md text-center md:text-left">
+              {step.description}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
