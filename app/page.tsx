4fa20e4d586843fee1565e6fd153bdcdbdbacd56
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2024-01-03 14:40:28 +0100

    feat(usp-section): add usp section

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { AboutSection } from "@/components/about-section";
 import { QuoteSection } from "@/components/quote-section";
 import { ServiceSection } from "@/components/service-section";
 import { WorkflowSection } from "@/components/workflow-section";
+import { UspSection } from "@/components/usp-section";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <QuoteSection />
       <ServiceSection />
       <WorkflowSection />
+      <UspSection />
       {/* <div className="flex flex-row justify-center gap-6 mt-8 items-center">
         <Link href="https://www.linkedin.com/in/marvin-splitt/">
           <Image src={LinkedIn} height={24} width={24} alt="LinkedIn" />
