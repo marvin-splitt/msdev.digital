19c1e7bcbffd9cfb12b8f7e8103b47f91d69233b
Marvin Splitt <marvin.splitt@gmx.net>
2024-03-08 14:48:28 +0200

    style: improve project card styling

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,4 +1,4 @@
-import { ProjectExcerpt } from "@/config/types";
+import { ProjectExcerpt } from "@/config/projects";
 import { cn } from "@/lib/utils";
 
 type ProjectCardProps = {
@@ -10,7 +10,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
 }) => {
   return (
     <div className="p-4 border border-black rounded-xl">
-      <p>{timeRange}</p>
+      <p className="text-accent text-sm">{timeRange}</p>
       <h5 className="font-semibold text-2xl mt-2.5">{title}</h5>
       <p className="text-lg mt-2.5">{description}</p>
       <div className="mt-4 flex flex-row gap-2 flex-wrap">
@@ -18,12 +18,12 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
           <span
             key={tag.name}
             className={cn(
-              "px-3.5 py-1 rounded-full font-semibold whitespace-nowrap",
-              {
-                "bg-[#1363811A] text-accent-blue": tag.color === "blue",
-                "bg-[#D2572D1A] text-accent": tag.color === "orange",
-                "bg-[#EEEEEE]": tag.color === "default",
-              }
+              "px-3.5 py-1 rounded-full text-xs font-semibold whitespace-nowrap bg-[#1363811A] text-accent-blue"
+              // {
+              //   "bg-[#1363811A] text-accent-blue": tag.color === "blue",
+              //   "bg-[#D2572D1A] text-accent": tag.color === "orange",
+              //   "bg-[#EEEEEE]": tag.color === "default",
+              // }
             )}
           >
             {tag.name}
